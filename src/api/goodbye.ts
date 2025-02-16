2f4aef97b19de0212e7f87eb61f330d46c73fede
petermichon <peter.michon@proton.me>
2025-02-16 15:53:23 +0100

    Setup Node

--- src/api/goodbye.ts
+++ src/api/goodbye.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from 'npm:@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(_: VercelRequest, res: VercelResponse) {
   return res.json({
