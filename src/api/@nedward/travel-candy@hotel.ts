af2431aceba9cc51ddf05220dcb68f09a69b2eda
petermichon <peter.michon@proton.me>
2025-02-18 23:19:57 +0100

    Restructure routes

--- src/api/@nedward/travel-candy@hotel.ts
+++ src/api/@nedward/travel-candy@hotel.ts
@@ -1,14 +1,8 @@
-// export function GET(_: Request) {
-//   return new Response(JSON.stringify({ name: 'Hotel' }), {
-//     headers: { 'Content-Type': 'application/json' },
-//   });
-// }
-
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 export function GET(_: Request) {
-  const svgFilePath = resolve(__dirname, 'image.svg');
+  const svgFilePath = resolve(__dirname, 'hotel.svg');
   const svgContent = readFileSync(svgFilePath, 'utf-8');
 
   return new Response(svgContent, {
---
+++ src/fetch.ts
@@ -0,0 +1,3 @@
+const url = 'https://nedward.vercel.app/api/@nedward/travel-candy@hotel';
+const res = await fetch(url);
+console.log(res);
