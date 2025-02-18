192299ebfbeeca44bc2269710e47fe447eb23e4e
petermichon <peter.michon@proton.me>
2025-02-18 23:26:24 +0100

    Update hotel.svg API

--- src/api/@nedward/hotel.svg.ts
+++ src/api/@nedward/hotel.svg.ts
@@ -1,6 +1,9 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
+// SVG Repo
+// Travel Theme Candy Vectors Collection
+// Hotel SVG Vector
 export function GET(_: Request) {
   const svgFilePath = resolve(__dirname, 'hotel.svg');
   const svgContent = readFileSync(svgFilePath, 'utf-8');
