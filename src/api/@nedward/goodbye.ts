2ffe97cd66d7634b0d14c72bb9fae06ee7f68d9d
petermichon <peter.michon@proton.me>
2025-02-19 14:11:04 +0100

    Clean codebase

--- src/api/@nedward/goodbye.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(_: VercelRequest, res: VercelResponse) {
-  return res.json({
-    message: `Goodbye!`,
-  });
-}
